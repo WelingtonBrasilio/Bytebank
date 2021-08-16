1baf1d3025e9d37e538c2f47abae887a335300be
Welington Pinheiro <welingtonbrasilio@gmail.com>
2021-08-16 19:24:48 -0300

    Criação de parametos de verificação para saque e deposito

--- Projeto ByteBank/index.js
+++ Projeto ByteBank/index.js
@@ -2,22 +2,47 @@
 class Cliente{
     nome;
     cpf;
-    agencia;
-    saldo;
 }
 
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
+//CC quer armazena e permite a retirada de dinheiro
+class ContaCorrente{
+    agencia;
+    _saldo = 0;
+    
+    sacar(valor){
+        if (this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
 
+    depositar(valor){
+        if(valor <= 0){
+            return;
+        }
+        this._saldo += valor;
+    }
+}
+
+
+const cliente1 = new Cliente();
 cliente1.nome = "Welington";
 cliente1.cpf = 12345678900;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 22345878909;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const contaCorrenteWelington = new ContaCorrente();
+contaCorrenteWelington.agencia = 1001;
+
+contaCorrenteWelington.depositar(100);
+
+const valorSacado = contaCorrenteWelington.sacar(50);
+console.log(valorSacado);
+
+
+
+console.log(contaCorrenteWelington);
+
 
