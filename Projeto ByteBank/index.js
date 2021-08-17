aa944af371d0983153e133f0b7bef08c93cf415e
Welington Pinheiro <welingtonbrasilio@gmail.com>
2021-08-17 19:23:38 -0300

    Criação de modulos para facilitar a leitura e organização

--- Projeto ByteBank/index.js
+++ Projeto ByteBank/index.js
@@ -1,48 +1,29 @@
-//Molde
-class Cliente{
-    nome;
-    cpf;
-}
-
-//CC quer armazena e permite a retirada de dinheiro
-class ContaCorrente{
-    agencia;
-    _saldo = 0;
-    
-    sacar(valor){
-        if (this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0){
-            return;
-        }
-        this._saldo += valor;
-    }
-}
-
+import { Cliente } from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
 cliente1.nome = "Welington";
 cliente1.cpf = 12345678900;
 
 const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 22345878909;
+cliente2.nome = "João";
+cliente2.cpf = 12398745605;
 
 const contaCorrenteWelington = new ContaCorrente();
+contaCorrenteWelington.cliente = cliente1;
 contaCorrenteWelington.agencia = 1001;
+contaCorrenteWelington.depositar(500);
 
-contaCorrenteWelington.depositar(100);
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 1002;
 
-const valorSacado = contaCorrenteWelington.sacar(50);
-console.log(valorSacado);
-
-
-
+contaCorrenteWelington.transferir(200, conta2);
+console.log(conta2);
 console.log(contaCorrenteWelington);
+
+
+
+
 
 
