99e2d8411a9eeaf821cbf366d9f4cb4bedb4c6f6
Welington Pinheiro <welingtonbrasilio@gmail.com>
2021-08-30 21:00:59 -0300

    Gerencia e define parametros, retornando-os para exibição

--- Projeto ByteBank/ContaCorrente.js
+++ Projeto ByteBank/ContaCorrente.js
@@ -1,10 +1,35 @@
+import { Cliente } from "./Cliente.js";
+
 //CC que armazena e permite a retirada de dinheiro
 export class ContaCorrente{
+    static numeroDeContas = 0;
     agencia;
-    cliente;
 
+    _cliente;
     _saldo = 0;
+
+//altera a regra de como um atributo pode ou não ser modificado
+    set cliente(novoValor){ 
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor; 
+        }
+    }
+
+//retorna seu valor para exibição
+    get cliente(){ 
+      return this._cliente;
+    }
+
+    get saldo(){
+      return this._saldo;
+    }
     
+    constructor(agencia, cliente){//inicia os atributos
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
+
     sacar(valor){
         if (this._saldo >= valor){
             this._saldo -= valor;
