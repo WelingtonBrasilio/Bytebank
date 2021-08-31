88620f06bb061ddc6f974faac7d1a296f5923953
Welington Pinheiro <welingtonbrasilio@gmail.com>
2021-08-30 21:06:43 -0300

    Adiciona e exibe os dados de cliente e conta

--- Projeto ByteBank/index.js
+++ Projeto ByteBank/index.js
@@ -1,26 +1,21 @@
 import { Cliente } from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = "Welington";
-cliente1.cpf = 12345678900;
+const cliente1 = new Cliente("Welington", 12345678900);
+const cliente2 = new Cliente("João", 12398745605);
 
-const cliente2 = new Cliente();
-cliente2.nome = "João";
-cliente2.cpf = 12398745605;
 
-const contaCorrenteWelington = new ContaCorrente();
-contaCorrenteWelington.cliente = cliente1;
-contaCorrenteWelington.agencia = 1001;
-contaCorrenteWelington.depositar(500);
+const conta1 = new ContaCorrente(1001, cliente1);
+const conta2 = new ContaCorrente(1002, cliente2);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 1002;
 
-contaCorrenteWelington.transferir(200, conta2);
-console.log(conta2);
-console.log(contaCorrenteWelington);
+conta1.depositar(500);
+
+console.log(conta1);
+console.log(cliente2);
+console.log("\nNúmero de contas criadas: " + ContaCorrente.numeroDeContas);
+
+
 
 
 
